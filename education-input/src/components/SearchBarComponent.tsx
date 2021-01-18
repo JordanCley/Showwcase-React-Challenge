172de0939fd447d4bee0592f47c5bbbc49101731
Jordan McQuiston <jordan@jordanmcquiston.com>
2021-01-17 18:52:16 -0800

    addressed type issues for even functions, added autoComplete function to see top 10 auto suggestions

--- education-input/src/components/SearchBarComponent.tsx
+++ education-input/src/components/SearchBarComponent.tsx
@@ -1,39 +1,58 @@
-import React, { useState} from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import API from "../API/api";
 
 interface School {
-    alphaTwoCode: string,
-    country: string,
-    domains: Array<string>,
-    name: string,
-    stateProvince: string | null,
-    webPages: Array<string>
+  alphaTwoCode: string;
+  country: string;
+  domains: Array<string>;
+  name: string;
+  stateProvince: string | null;
+  webPages: Array<string>;
 }
 
 export default function SearchBarComponent() {
-    
-
   const [schoolNameState, setSchoolNameState] = useState("");
   const [schoolSuggestionsListState, setSchoolSuggestionsListState] = useState<
     Array<School>
   >([]);
 
-  const handleChange = (event: any) => {
-    const { value } = event.target;
+    const autoCompleteList = (schoolList: Array<School>) => {
+      const schoolArray: School[] = [];
+      if (schoolList.length < 7) {
+        for (let i = 0; i < schoolList.length; i++) {
+          console.log(schoolList[i]);
+          schoolArray.push(schoolList[i]);
+        console.log(`schoolArrayUnder7: ${schoolArray}`);
+
+        }
+        return schoolArray;
+      } else {
+        for (let i = 0; i < 7; i++) {
+          console.log(schoolList[i]);
+          schoolArray.push(schoolList[i]);
+        }
+        console.log(`schoolArray: ${schoolArray}`);
+        console.log(`schoolArrayOver7: ${schoolArray[1]}`);
+        return schoolArray;
+      }
+    };
+
+
+  const handleChange = (event: ChangeEvent) => {
+    const { value } = event.target as HTMLInputElement;
     setSchoolNameState(value);
-    if (value.length > 2) {
-      return API.searchSchoolName(value)
-        .then((res) => {
-          setSchoolSuggestionsListState(res.data);
-          console.log(schoolSuggestionsListState);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+    return API.searchSchoolName(schoolNameState)
+      .then((res) => {
+        setSchoolSuggestionsListState(res.data);
+        console.log("schoolList: ");
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     return API.searchSchoolName(schoolNameState)
       .then((res) => {
@@ -60,11 +79,13 @@ export default function SearchBarComponent() {
       </form>
 
       <ul>
-        {schoolSuggestionsListState.length > 1
-          ? schoolSuggestionsListState.map((school: School) => {
-              return <li>{school.name}</li>;
-            })
-          : ( <li></li> )}
+        {schoolSuggestionsListState.length ? (
+          autoCompleteList(schoolSuggestionsListState).map((school: School) => {
+            return <li>{school.name}</li>;
+          })
+        ) : (
+          <></>
+        )}
       </ul>
     </div>
   );
